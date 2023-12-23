78404af21ee22d2462c9eafcd5bc082805376da2
imbeltagy <justbeltagy@gmail.com>
2023-12-23 22:46:26 +0200

    fixed: single question page

--- src/pages/singleQuestionPage/Index.jsx
+++ src/pages/singleQuestionPage/Index.jsx
@@ -9,33 +9,34 @@ import fetchApi from "/src/app/fetchApi/Index";
 const SingleQuestionPage = () => {
   const [questionExist, setQuestionExist] = useState("loading");
   const { questionID } = useParams();
+  const [id, setId] = useState(); // id: question date
   const dispatch = useDispatch();
-  const { username, isLogged, isFetching } = useSelector((state) => state.user);
+  const isFetching = useSelector((state) => state.user.isFetching);
 
   // Fetch Question
   useEffect(() => {
     const getQuestions = async () => {
       // Res For Question
       const res = await fetchApi(
-        `get_questions?question_id=${[questionID].join("&question_id=")}&current_user_username=${
-          isLogged ? username : "none"
-        }`,
+        `get_questions?question_id=${[questionID].join("&question_id=")}&current_user_username=none`,
         "GET"
       );
       if (res.success) {
         // Save Questions as a State
         const question = res.data[0];
+        const date = new Date(question.publish_date).getTime();
         const questionData = {
           id: questionID,
+          date,
           username: question.author_username,
           displayName: question.author_display_name,
           title: question.question_title,
           content: question.question_content,
-          date: new Date(question.publish_date).getTime(),
           isSolved: question.solved_state,
           isSaved: false,
         };
-        dispatch(pushQuestion({ id: questionID, data: questionData }));
+        dispatch(pushQuestion(questionData));
+        setId(date);
         setQuestionExist(true);
       }
     };
@@ -47,9 +48,7 @@ const SingleQuestionPage = () => {
   if (questionExist)
     return (
       <Suspense>
-        <Box pb={4}>
-          <FullQuestion id={questionID} />
-        </Box>
+        <Box pb={4}>{id ? <FullQuestion id={id} /> : null}</Box>
       </Suspense>
     );
 
