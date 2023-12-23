977aca6fe8d36c30a90b70f6ed54178fbd97d99d
imbeltagy <justbeltagy@gmail.com>
2023-12-23 09:32:02 +0200

    fix: connected AnswerInput to API

--- src/common/question/components/answersSection/InputAnswer.jsx
+++ src/common/question/components/answersSection/InputAnswer.jsx
@@ -1,10 +1,11 @@
 import { useTheme } from "@emotion/react";
 import { Send } from "@mui/icons-material";
-import { Box, IconButton, InputBase, Link, Stack } from "@mui/material";
+import { Alert, Box, IconButton, InputBase, Link, Stack } from "@mui/material";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { pushAnswers } from "/src/features/questions/questionsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Link as RouterLink } from "react-router-dom";
+import fetchApi from "/src/app/fetchApi/Index";
 
 const InputAnswer = ({ id }) => {
   const dispatch = useDispatch();
@@ -12,7 +13,8 @@ const InputAnswer = ({ id }) => {
   const [inputVal, setInputVal] = useState("");
   const [isShiftPressed, setIsShiftPressed] = useState(false);
   const ref = useRef();
-  const { username: currentUser, isLogged } = useSelector((state) => state.user);
+  const { username: currentUser, displayName, isLogged } = useSelector((state) => state.user);
+  const [errorMsg, setErrorMsg] = useState();
 
   // Focus On Input on Mount
   useEffect(() => {
@@ -20,11 +22,46 @@ const InputAnswer = ({ id }) => {
   }, [isLogged]);
 
   const publishAnswer = useCallback(() => {
-    const tempID = Math.random();
-    dispatch(pushAnswers({ id, data: { [tempID]: { username: currentUser, content: ref.current.value } } }));
-    ref.current.value = "";
-    // Send Answer To API
+    // save Input value
+    const content = ref.current.value;
+
+    const sendAnswer = async () => {
+      // Update The UI Till Calling API
+      // Generate temp ID for the answer till getting it again from server
+      const tempID = Math.random();
+
+      // save Answer As Local State
+      dispatch(
+        pushAnswers({
+          questionId: id,
+          answers: { [tempID]: { username: currentUser, displayName, date: new Date().getTime(), content } },
+        })
+      );
+
+      // Reset Input Value
+      ref.current.value = "";
+
+      // Push Answer To Server
+      const res = await fetchApi("answer_question", "POST", {
+        question_id: id,
+        answer_author: currentUser,
+        content,
+      });
+
+      if (res.success) {
+        // Remove Error if Exist
+        setErrorMsg(null);
+      } else {
+        // Set Error Message
+        setErrorMsg(
+          "Error with sending answer. Check your connections and try again.\nIf this error keeps happen please contact us."
+        );
+      }
+    };
+    content != "" && sendAnswer();
   }, []);
+
+  if (errorMsg) return <Alert severity="error">{errorMsg}</Alert>;
 
   return (
     <Stack
