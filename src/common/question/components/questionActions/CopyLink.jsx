c102da0ad3ef602b8dcda011de1932f6f0a62f43
imbeltagy <justbeltagy@gmail.com>
2023-12-24 20:24:17 +0200

    fix: copy link button now copies the question api id not state id

--- src/common/question/components/questionActions/CopyLink.jsx
+++ src/common/question/components/questionActions/CopyLink.jsx
@@ -2,11 +2,12 @@ import { Link } from "@mui/icons-material";
 import { IconButton, Tooltip } from "@mui/material";
 import { useRef } from "react";
 import { open as openNotification } from "/src/features/notification/notificationSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const CopyLink = ({ id }) => {
   const dispatch = useDispatch();
   const ref = useRef();
+  const questionApiId = useSelector((state) => state.questions.savedQuestions[id].id);
 
   const handleCopy = () => {
     ref.current.select();
@@ -24,7 +25,7 @@ const CopyLink = ({ id }) => {
 
       {/* Input To Hold The Link */}
       <input
-        value={`${location.host}/question/${id}`}
+        value={`${location.host}/question/${questionApiId}`}
         readOnly
         ref={ref}
         tabIndex="-1"
